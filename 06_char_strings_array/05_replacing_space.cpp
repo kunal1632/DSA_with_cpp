89003b819970ec7eadadf2b34bbaf6c65a1240e9
kunal1632 <kunaldhand1632@gmail.com>
2023-08-18 17:25:35 +0530

    bug fix

--- 06_char_strings_array/05_replacing_space.cpp
+++ 06_char_strings_array/05_replacing_space.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+string replaceSpace(string str)
+{
+    string str2 = "@40";
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == ' ')
+        {
+            str.replace(i, 1, str2);
+        }
+    }
+    return str;
+}
 
 int main()
 {
     string str = "";
 
     cout << "Enter the string: " << endl;
-    cin.getline(str, 50);
+    getline(cin, str);
+    cout << "original string: " << str << endl;
+    string str2 = replaceSpace(str);
+    cout << "modified string: " << str2 << endl;
 }
